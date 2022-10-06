998244ace9b09d5fd71f11343663ab43eafa4cff
VicenteKrause <94655221+VicenteKrause@users.noreply.github.com>
2022-10-06 18:17:07 -0300

    login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },  {
+  },
+  {
     path: 'register',
     loadChildren: () => import('./Pages/register/register.module').then( m => m.RegisterPageModule)
   },
@@ -19,6 +20,12 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./Pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./Pages/home/home.module').then( m => m.HomePageModule)
+  },
+
+
 
   // {
   //   path: '/register',
